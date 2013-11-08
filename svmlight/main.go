7736945e4d8fc01f48e91a284db23f7e78d670a9
Antoine <antoine@ubuntu.(none)>
2013-11-08 15:51:19 -0500

    Finished Learn

--- svmlight/main.go
+++ svmlight/main.go
@@ -1,18 +1,32 @@
 package main
 
 import (
-
+	"exec"
+	"fmt"
 )
 
-func Learn(){
+func Learn(trainFile, modelFile string, c float64, j float64, d int) {
+	t := 0
+	if d != 0 {
+		t := 1
+	}
+	if c == 0. {
+		c = 1.
+	}
+	if j == 0. {
+		j = 1.
+	}
+	out, _ := exec.Command("svm_learn", "-c", fmt.Sprintf("%f", c),
+		"-j", fmt.Sprintf("%f", j),
+		"-t", fmt.Sprintf("%f", t),
+		"-d", fmt.Sprintf("%f", d))
+	return fmt.Sprintf("%s\n", out)
+}
+
+func Classify() {
 
 }
 
-func Classify(){
-
-}
-
-func GetAccuracy(){
-
+func GetAccuracy() {
 
 }
